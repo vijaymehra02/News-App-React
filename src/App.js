03bf96e3f9e46aa8e27f8670e4c58f15ea178743
vijay bahadur singh mehra <vijaybahadursinghmehra@gmail.com>
2024-03-19 01:53:39 +0530

    install react router

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import './App.css';
 import React, { Component } from 'react'
 import Nav from './Components/Nav';
 import Newscomponent from './Components/Newscomponent';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+}from "react-router-dom"
 
 
 export default class App extends Component {
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -16,16 +16,33 @@ export default class Nav extends Component {
                                     <a className="nav-link active" aria-current="page" href="#">Home</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">Link</a>
+                                    <a className="nav-link" href="/Business">Business</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/Etertainment">Etertainment</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/General">General</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/Health">Health</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/Science">Science</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/Sports">Sports</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="Technology">Technology</a>
                                 </li>
                                 <li className="nav-item dropdown">
                                     <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Dropdown
-                                    </a>
+                                        Dropdown</a>
                                     <ul className="dropdown-menu">
                                         <li><a className="dropdown-item" href="#">Action</a></li>
                                         <li><a className="dropdown-item" href="#">Another action</a></li>
-                                        <li><hr className="dropdown-divider"/></li>
+                                        <li><hr className="dropdown-divider" /></li>
                                         <li><a className="dropdown-item" href="#">Something else here</a></li>
                                     </ul>
                                 </li>
@@ -34,8 +51,8 @@ export default class Nav extends Component {
                                 </li>
                             </ul>
                             <form className="d-flex" role="search">
-                                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                                    <button className="btn btn-outline-success" type="submit">Search</button>
+                                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                                <button className="btn btn-outline-success" type="submit">Search</button>
                             </form>
                         </div>
                     </div>
